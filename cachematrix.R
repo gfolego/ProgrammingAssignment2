396ae6e7d671345143dbd1b5a52415e78951e888
Guilherme A. Fôlego <gfolego@gmail.com>
2015-04-25 15:42:21 -0300

    Add license information

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,24 @@
 ## R Programming - Programming Assignment 2 [rprog-006]
 
+
+# cachematrix.R
+# Copyright (C) 2014 Guilherme A. Fôlego (gfolego@gmail.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 ## These functions create an object that has a matrix and its inverse.
 ## The inverse is cached in this object and
 ## 	it only needs to be calculated once,
