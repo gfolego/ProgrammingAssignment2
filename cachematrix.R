2a558e36e7267a58765031e1d94bc22d69731e82
Guilherme A. Fôlego <gfolego@gmail.com>
2016-07-15 13:06:29 -0300

    Update license information

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,7 @@
 
 
 # cachematrix.R
-# Copyright (C) 2014 Guilherme A. Fôlego (gfolego@gmail.com)
+# Copyright (C) 2014 Guilherme Folego (gfolego@gmail.com)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
